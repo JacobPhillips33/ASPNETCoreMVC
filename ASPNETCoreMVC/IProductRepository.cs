2d8574d803400db5e0e9857f9bda945e2c3b96bd
Jacob Phillips <jakephillips.wv@gmail.com>
2022-10-19 17:19:23 -0500

    completed intro exercises

---
+++ ASPNETCoreMVC/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using ASPNETCoreMVC.Models;
+
+namespace ASPNETCoreMVC
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+    }
+}
